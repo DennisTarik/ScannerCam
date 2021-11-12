8405d6c8316a855675edf9eaf776ba30daf112f6
DennisTarik <dm499555@gmail.com>
2021-11-12 15:24:12 +0100

    :bug: minor doc preview change

--- src/app/components/DocPreview/DocPreview.stories.tsx
+++ src/app/components/DocPreview/DocPreview.stories.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import DocPreview from './DocPreview';
+import DocumentList from './DocPreview';
 
-export default { component: DocPreview, title: 'Components/DocPreview' };
+export default { component: DocumentList, title: 'Components/DocPreview' };
 
-export const Page1 = (): JSX.Element => (
-  <DocPreview title="HomePage" text="Test test test" />
-);
+export const Page1 = (): JSX.Element => <DocumentList />;
 
-export const Page2 = (): JSX.Element => (
-  <DocPreview title="ScanPage" text="123123123" />
-);
+export const Page2 = (): JSX.Element => <DocumentList />;
 
-export const Page3 = (): JSX.Element => (
-  <DocPreview title="FolderPage" text="123123123" />
-);
+export const Page3 = (): JSX.Element => <DocumentList />;
